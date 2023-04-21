66a1ae5c02ffb1e219191e3cf16334b3db7357cd
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2023-04-21 12:35:04 +0530

    ui

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import ChatBody from "./components/ChatBody";
+
+function App() {
+  return (
+    <div className="App">
+      <ChatBody />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ChatBody.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChatList from './ChatList/ChatLists'
+import ChatContent from './ChatContent/ChatContent';
+
+const ChatBody = () => {
+  return (
+    <>
+    <ChatContent />
+    <ChatList />
+    </>
+  )
+}
+
+export default ChatBody
