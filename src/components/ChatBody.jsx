97a5bb2bbec28fbf7d2a72a09f51bdc3e1c2c7e1
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2023-04-22 17:37:51 +0530

    chat body responsive

--- src/components/ChatBody.jsx
+++ src/components/ChatBody.jsx
@@ -6,14 +6,14 @@ import ChatContent from './ChatContent/ChatContent';
 const ChatBody = () => {
   return (
     <>
-    <Grid cotainer sx={{display:"flex", flexDirection:"row"}}>
-      <Grid item width="20%">
-        <ChatList />
+      <Grid container sx={{ display: "flex", flexDirection: { xs: "column", sm: "column", md: "row" } }}>
+        <Grid item xs={12} md={2} lg={2}>
+          <ChatList />
+        </Grid>
+        <Grid item xs={12} md={10} lg={10}>
+          <ChatContent />
+        </Grid>
       </Grid>
-      <Grid item width="80%">
-        <ChatContent />
-      </Grid>
-    </Grid>
     </>
   )
 }
