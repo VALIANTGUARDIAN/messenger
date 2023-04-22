cbb280c287d93056f5c34de25acc393b293e9dfe
VALIANTGUARDIAN <singhdigvijay703@gmail.com>
2023-04-22 14:59:39 +0530

    0

--- src/components/ChatContent/Chats/ChatPrompt.jsx
+++ src/components/ChatContent/Chats/ChatPrompt.jsx
@@ -2,59 +2,46 @@ import { Avatar, Box, Paper } from "@mui/material";
 import React from "react";
 import { userChat } from "../Chats/UserChats";
 
-const UserChat = (props) => {
-  const { user, msg, image } = props;
+const UserChat = ({ user, msg, image }) => {
   const isOther = user === "other";
 
   return (
-    <div
-      style={{ animationDelay: `0.8s` }}
-      className={`chat__item ${isOther ? "other" : ""}`}
-    >
-      <Paper elevation={4}>
-        <Box sx={{ display: "flex", alignItems: "center", p: 1 }}>
-          {isOther ? (
-            <Avatar
-              sx={{ mr: 1 }}
-              variant="circular"
-              alt="User Image"
-              src={image}
-            />
-          ) : null}
-          <Box sx={{ flexGrow: 1 }}>
-            <div className="chat__msg">{msg}</div>
-            <div className="chat__meta">
-              <span>16 mins ago</span>
-              <span>Seen 1.03PM</span>
-            </div>
-          </Box>
-          {!isOther ? (
-            <Avatar
-              sx={{ ml: 1 }}
-              variant="circular"
-              alt="User Image"
-              src={image}
-            />
-          ) : null}
+    <Box sx={{width:"20rem", float: isOther ? "left" : "right" }} >
+      <Paper elevation={4} sx={{ display: "flex", alignItems: "center", p: 1}}>
+        {isOther && (
+          <Avatar
+            sx={{ mr: 1 }}
+            variant="circular"
+            alt="User Image"
+            src={image}
+          />
+        )}
+        <Box sx={{ flexGrow: 1 }}>
+          <div className="chat__msg">{msg}</div>
+          <div className="chat__meta">
+            <span>16 mins ago</span>
+            <span>Seen 1.03PM</span>
+          </div>
         </Box>
+        {!isOther && (
+          <Avatar
+            sx={{ ml: 1 }}
+            variant="circular"
+            alt="User Image"
+            src={image}
+          />
+        )}
       </Paper>
-    </div>
-  );
-};
-
-const ChatPrompt = () => {
-  return (
-    <Box className="chat__list">
-      {userChat.map((chat) => (
-        <UserChat
-          key={chat.key}
-          user={chat.type}
-          msg={chat.msg}
-          image={chat.image}
-        />
-      ))}
     </Box>
   );
 };
+
+const ChatPrompt = () => (
+  <Box className="chat__list">
+    {userChat.map(({ key, type, msg, image }) => (
+      <UserChat key={key} user={type} msg={msg} image={image} />
+    ))}
+  </Box>
+);
 
 export default ChatPrompt;
